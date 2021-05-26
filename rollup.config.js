813ce43eb89d08584c265e26bf475b241b803655
DavideViolante <fungtq3@gmail.com>
2021-05-26 18:23:10 +0200

    Create bundle in dist folder instead of bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,6 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/chartjs-plugin-labels.js',
   output: [
-    { file: 'build/chartjs-plugin-labels.min.js', format: 'umd', name: 'chartjs-plugin-labels', plugins: [terser()] },
+    { file: 'dist/chartjs-plugin-labels.min.js', format: 'umd', name: 'chartjs-plugin-labels', plugins: [terser()] },
   ],
 };
